6df8606f07a22a7c83780ac8570b6729df29ef6f
NaughtySora <naughtysora@proton.me>
2026-05-01 16:53:10 +0300

    refactor, test, coverage, types

--- main.d.ts
+++ main.d.ts
@@ -1,11 +1,15 @@
-interface Options {
-  timeout?: number;
-  interval?: number;
-}
+type Tty = (key: any, value: number) => Tty;
+type Set = <F extends (...args: any[]) => any, A extends Parameters<F>>
+  (key: any, fn: F, ...args: A) => Set;
 
-type Factory = (...args: any[]) => any;
-type LazyFn = (factory: Factory, options?: Options) => void;
-type LazyAPI = { reset(): void, [Symbol.dispose](): void };
-type Lazy = LazyFn & LazyAPI;
+interface Lazy {
+  set: Set
+  get(key: any): any;
+  tty: Tty;
+  clear(): void;
+  del(key: any): boolean;
+  has(key: any): boolean;
+  size: number;
+}
 
 export const lazy: Lazy;
--- test/index.js
+++ test/index.js
@@ -1,70 +1,95 @@
 "use strict";
 
+const { async } = require("naughty-util");
 const { lazy } = require("../main");
-const assert = require("node:assert");
+const assert = require("node:assert/strict");
 const { describe, it } = require("node:test");
 const timers = require("node:timers/promises");
 
-const count = (fn) => {
-  const inner = () => {
-    inner.counter++;
-    return fn();
-  };
-  inner.counter = 0;
-  return inner;
+const createCount = () => {
+  const count = fn => (count.counter++, fn());
+  count.counter = 0;
+  return count;
 };
 
-describe("const", () => {
-  it("plain", () => {
-    const actual = { a: 1 };
-    const factory = count(() => actual);
-    const get = lazy(factory);
-    const expected = get();
-    (get(), get(), get());
-    assert.deepStrictEqual(expected, actual);
-    assert.deepStrictEqual(factory.counter, 1);
+describe("lazy", () => {
+  it("simple", () => {
+    const count = createCount();
+    const KEY = "abc";
+    lazy.set(KEY, () => { })
+    (KEY, count, () => ({ a: 1 }));
+    const expected = lazy.get(KEY);
+    const expected2 = lazy.get(KEY);
+    (lazy.get(KEY), lazy.get(KEY), lazy.get(KEY));
+    assert.deepEqual(expected, expected2);
+    assert.equal(count.counter, 1);
+  });
+
+  it("tty", async () => {
+    const count = createCount();
+    const KEY = "123";
+    lazy.set(KEY, count, () => ({ a: Math.random() }));
+    lazy.tty(KEY, 500);
+    const expected = lazy.get(KEY);
+    const expected2 = lazy.get(KEY);
+    (lazy.get(KEY), lazy.get(KEY), lazy.get(KEY));
+    assert.deepEqual(expected, expected2);
+    assert.equal(expected.a, expected2.a);
+    assert.equal(count.counter, 1);
+    await async.pause(500);
+    const expected3 = lazy.get(KEY);
+    assert.notDeepEqual(expected3, expected2);
+    assert.notEqual(expected3.a, expected2.a);
+    assert.equal(count.counter, 2);
+    lazy.tty(KEY, 250);
+    await async.pause(250);
+    const expected4 = lazy.get(KEY);
+    assert.notDeepEqual(expected3, expected4);
+    assert.notEqual(expected4.a, expected3.a);
+    assert.equal(count.counter, 3);
+
+    lazy.tty("smth", 500);
+  });
+
+  it('clear', () => {
+    lazy.clear();
+    assert.equal(lazy.get('abc'), undefined);
+    assert.equal(lazy.get('123'), undefined);
+    lazy.set('321', () => { });
+    lazy.tty('321', 500);
+    lazy.clear();
+    assert.equal(lazy.get('321'), undefined);
+
+  });
+
+  it('has', () => {
+    const KEY = "+";
+    lazy.set(KEY, () => { });
+    assert.ok(lazy.has(KEY));
+    assert.ok(!(lazy.has(KEY + KEY)));
+  });
+
+  it('delete', () => {
+    const KEY = "-";
+    lazy.set(KEY, () => { });
+    lazy.tty(KEY, 1234);
+    assert.ok(lazy.has(KEY));
+    lazy.delete(KEY);
+    assert.ok(!lazy.has(KEY));
+    assert.ok(!lazy.delete('-=-'));
   });
 });
 
-describe("timeout", async () => {
-  const actual = { a: 1 };
-  const factory = count(() => actual);
-  const get = lazy(factory, { timeout: 1000 });
-  const data = get();
-  assert.deepStrictEqual(data, actual);
-  await timers.setTimeout(1500);
-  const data2 = get();
-  assert.deepStrictEqual(data2, actual);
-  assert.deepStrictEqual(factory.counter, 2);
-});
-
-describe("interval/dispose", async () => {
-  const actual = { a: 1 };
-  const factory = count(() => actual);
-  const get = lazy(factory, { interval: 500 });
-  const data = get();
-  assert.deepStrictEqual(data, actual);
-  await timers.setTimeout(750);
-  const data2 = get();
-  assert.deepStrictEqual(data2, actual);
-  await timers.setTimeout(750);
-  const data3 = get();
-  assert.deepStrictEqual(data3, actual);
-  assert.deepStrictEqual(factory.counter, 3);
-  get[Symbol.dispose]();
-});
-
-describe("node 24.x using", async () => {
-  const actual = { a: 1 };
-  const factory = count(() => actual);
-  using get = lazy(factory, { interval: 500 });
-  const data = get();
-  assert.deepStrictEqual(data, actual);
-  await timers.setTimeout(750);
-  const data2 = get();
-  assert.deepStrictEqual(data2, actual);
-  await timers.setTimeout(750);
-  const data3 = get();
-  assert.deepStrictEqual(data3, actual);
-  assert.deepStrictEqual(factory.counter, 3);
-});
+//   const actual = { a: 1 };
+//   const factory = count(() => actual);
+//   using get = lazy(factory, { interval: 500 });
+//   const data = get();
+//   assert.deepEqual(data, actual);
+//   await timers.setTimeout(750);
+//   const data2 = get();
+//   assert.deepEqual(data2, actual);
+//   await timers.setTimeout(750);
+//   const data3 = get();
+//   assert.deepEqual(data3, actual);
+//   assert.deepEqual(factory.counter, 3);
+// });
